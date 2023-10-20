0dc7bc37312b90115f6ae07951698a7ce51cfc3d
Nazik2020 <148340424+Nazik2020@users.noreply.github.com>
2023-10-20 19:09:16 +0530

    added new print line

--- main.c
+++ main.c
@@ -1,46 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <ctype.h>
 #include <string.h>
-#include <math.h>
 
-int main (){
-    float num1;
-    float num2;
-    char op;
-    printf ("___________________C   A   L   C   U   L   A   T   O  R______________________\n\n");
-    printf ("Enter a number     :");
-    scanf (" %f",&num1 );
+int main()
+{
 
-    printf ("Enter an operator  :");
-    scanf (" %c",&op);
+  int a;
+  int books[5];
+  int total = 0.0;
 
-    printf ("Enter a number     :");
-    scanf ("%f", &num2);
+  for (a = 0; a < 5; a++)
+  {
+    printf(" How many books you read per day %d  :", a + 1);
+    scanf("%d", &books[a]);
 
-    if (op == '+'){
-        printf (" %f",num1+num2 );
-
-    }
-
-    else if (op == '-'){
-        printf (" %f",num1-num2 );
-
-    }
-
-     else if (op == '/'){
-        printf (" %f",num1/num2 );
-
-     }
-      else if (op == '*'){
-        printf (" %f",num1 * num2 );
-      }
-
-      else {
-        printf ("Error\a");
-      }
-
-
-
-
+    total += books[a];
+  }
+  printf("Total books  : %d", total);
+  printf("Total books  : %d", total);
 }
